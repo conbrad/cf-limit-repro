c1992a86e59caa6ec0a0106b2b6794cc7f1199c2
Conor Brady <con.brad@gmail.com>
2024-08-20 22:49:48 -0700

    simplify

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,38 @@
 export default {
 	async fetch(request: Request, env: Env): Promise<Response> {
-	  try {
-		const url = new URL(request.url)
-		switch (url.pathname) {
-		  case '/ws':
-			return env.websocket_server.get(env.websocket_server.newUniqueId()).fetch(request);
-		  default:
-			return new Response("Not authorized", { status: 401 })
+		try {
+			const url = new URL(request.url)
+			switch (url.pathname) {
+				case '/ws':
+					return env.websocket_server.get(env.websocket_server.newUniqueId()).fetch(request);
+				default:
+					return new Response("Not authorized", { status: 401 })
+			}
+		} catch (err) {
+			console.log(err)
+			return new Response("error")
 		}
-	  } catch (err) {
-		console.log(err)
-		return new Response("error")
-	  }
 	},
-}
+
+	async queue(batch: MessageBatch, env: Env): Promise<void> {
+		for (const message of batch.messages) {
+			try {
+				const importMessage = message.body as ImportMessage
+				const response = await fetch(importMessage.url)
+				const body = await response.text()
+				console.info(`Consumer fetched: ${importMessage.url}`)
+
+				if (importMessage.ws_id) {
+					await env.websocket_server
+						.get(env.websocket_server.idFromString(importMessage.ws_id))
+						.fetch(new Request(importMessage.url,
+							{ method: "POST", body: JSON.stringify({ content: body }) }));
+				}
+			} catch (e) {
+				console.error(e)
+			}
+		}
+	}
+} satisfies ExportedHandler<Env>;
 
 export { WebsocketServer } from "./websocket-server";
--- src/websocket-server.ts
+++ src/websocket-server.ts
@@ -1,9 +1,5 @@
 import { DurableObject } from "cloudflare:workers";
 
-export interface Env {
-	db: D1Database
-}
-
 export class WebsocketServer extends DurableObject<Env> {
 	constructor(private readonly state: DurableObjectState, env: Env) {
 		super(state, env);
@@ -20,7 +16,13 @@ export class WebsocketServer extends DurableObject<Env> {
 	async webSocketMessage(ws: WebSocket, message: string | ArrayBuffer) {
 		const req = JSON.parse(message.toString());
 		console.log("Message received")
-		await this.env.db.prepare(`insert into links (id, url) values (?, ?)`).bind(crypto.randomUUID(), req.url).run()
+		try {
+			await this.env.test_queue_binding.send({ url: req.url, ws_id: this.ctx.id.toString() });
+			ws.send(JSON.stringify({ url: req.url }))
+		} catch (e) {
+			console.log(e)
+			ws.send(JSON.stringify({ url: req.url, error: JSON.stringify(e) }))
+		}
 		ws.send(JSON.stringify({ error: false, url: req.url }))
 	}
 }
