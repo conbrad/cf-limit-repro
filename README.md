f7e2fd6a0088fcead9cdbeee71d9f434e2b6c0c5
Conor Brady <con.brad@gmail.com>
2024-08-21 22:05:24 -0700

    Reproduced issue

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-Cloudflare workers websocket server example -- sending thousands of urls to a websocket that attempts to fetch and return.
+Cloudflare workers websocket server example -- sending thousands of urls to a websocket that attempts to fetch and return. 
+The browser eventually receives a close event after a few minutes with an error code of 1006.
 
-Deploy with `npx wrangler deploy src/index.ts`
+1. Deploy with `npx wrangler deploy`
+2. Open `src/client/index.html` in your browser
+3. Open devtools to console view
+3. Select `src/client/raindrop-backup-may-2024.html` as the file to upload
+4. Logs of sent and received messages between browser and DO Websocket server flow
+5. After a few minutes you should see `Closed websocket, code: 1006, event: ` with no more logs coming through
--- src/websocket-server.ts
+++ src/websocket-server.ts
@@ -5,9 +5,25 @@ export class WebsocketServer extends DurableObject<Env> {
 		super(state, env);
 	}
 
-	async fetch(_: Request): Promise<Response> {
+	async fetch(request: Request): Promise<Response> {
+		if (request.method === "POST" && request.body) {
+			const pageContentMessage: ImportMessage = await request.json()
+			const websockets = this.state.getWebSockets("storeader")
+			if (websockets.length > 0) {
+				websockets[0].send(JSON.stringify({ url: request.url, content: pageContentMessage.content }))
+				return new Response(null, { status: 200 })
+			}
+			console.error(`Failed to lookup websocket to send content to: ${JSON.stringify(request)}`)
+			return new Response(null, { status: 500 })
+		}
+
+		const upgradeHeader = request.headers.get("Upgrade")
+		if (upgradeHeader !== "websocket") {
+			return new Response("Expected websocket", { status: 400 })
+		}
+
 		const [client, server] = Object.values(new WebSocketPair());
-		this.state.acceptWebSocket(server);
+		this.state.acceptWebSocket(server, ["storeader"]);
 		return new Response(null, {
 			status: 101,
 			webSocket: client,
@@ -16,6 +32,12 @@ export class WebsocketServer extends DurableObject<Env> {
 	async webSocketMessage(ws: WebSocket, message: string | ArrayBuffer) {
 		const req = JSON.parse(message.toString());
 		console.log("Message received")
+		
+		if (req.content) {
+			ws.send(req.content)
+			return
+		}
+
 		try {
 			await this.env.test_queue_binding.send({ url: req.url, ws_id: this.ctx.id.toString() });
 			ws.send(JSON.stringify({ url: req.url }))
